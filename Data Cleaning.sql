a21008c062cc9ee8ca15b530aef3ff6d78d1ead4
Muhammad Bilal Saeed <bilalsaeed18.bs@gmail.com>
2024-01-02 00:47:53 +0500

    Update Data Cleaning

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,4 +1,106 @@
 /* Data Cleaning in SQL */
 
+select * from NashvilleHousing
+
+-- Apply Standard Date format
+select SaleDate, CONVERT(Date, SaleDate) from NashvilleHousing
+
+-- Updating the SaleDate to the Nashville Housing Table
+Update NashvilleHousing
+set SaleDate = CONVERT(Date, SaleDate)
+
+-- some time it doesn;t work so we will alter the column
+Alter Table NashvilleHousing
+add SaleDateConverted Date;
+
+Update NashvilleHousing
+set SaleDateConverted = CONVERT(Date, SaleDate)
+
+select SaleDateConverted from NashvilleHousing
 
 
+--	Populate property Adress
+select PropertyAddress from NashvilleHousing
+where PropertyAddress is null
+
+select * from NashvilleHousing
+--where PropertyAddress is null
+
+
+select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+from NashvilleHousing a
+JOIN NashvilleHousing b
+on a.ParcelID = b.ParcelID
+AND a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is null
+
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+from NashvilleHousing a
+JOIN NashvilleHousing b
+on a.ParcelID = b.ParcelID
+AND a.[UniqueID ] <> b.[UniqueID ]
+where a.PropertyAddress is null
+
+
+-- Breaking Addess into multiple column (Address, City, State)
+
+select PropertyAddress from NashvilleHousing
+
+Select SUBSTRING(PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1)  as Address,
+SUBSTRING(PropertyAddress,  CHARINDEX(',',PropertyAddress) + 1, LEN(PropertyAddress)) as City 
+from NashvilleHousing
+
+
+Alter Table NashvilleHousing
+add propertySpilitAddress nvarchar(255);
+
+Update NashvilleHousing
+set propertySpilitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',',PropertyAddress)-1)
+
+
+Alter Table NashvilleHousing
+add propertySpilitCity nvarchar(255);
+
+Update NashvilleHousing
+set propertySpilitCity = SUBSTRING(PropertyAddress,  CHARINDEX(',',PropertyAddress) + 1, LEN(PropertyAddress))
+
+
+-- Spiliting owner address
+
+select OwnerAddress from NashvilleHousing
+
+select
+PARSENAME(REPLACE(OwnerAddress,',','.'),3),
+PARSENAME(REPLACE(OwnerAddress,',','.'),2),
+PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+from NashvilleHousing
+
+
+-- UPDATE COLUMNS IN TABLE
+
+Alter Table NashvilleHousing
+add OwnerSpilitAddress nvarchar(255);
+
+Update NashvilleHousing
+set OwnerSpilitAddress = PARSENAME(REPLACE(OwnerAddress,',','.'),3)
+
+---
+Alter Table NashvilleHousing
+add OwnerSpilitCity nvarchar(255);
+
+Update NashvilleHousing
+set OwnerSpilitCity = PARSENAME(REPLACE(OwnerAddress,',','.'),2)
+
+---
+
+Alter Table NashvilleHousing
+add OwnerSpilitState nvarchar(255);
+
+Update NashvilleHousing
+set OwnerSpilitState = PARSENAME(REPLACE(OwnerAddress,',','.'),1)
+
+
+
+select * from NashvilleHousing
